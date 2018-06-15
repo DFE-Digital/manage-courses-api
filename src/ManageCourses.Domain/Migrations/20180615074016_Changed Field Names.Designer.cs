f6461b3acc50f114f6b89c66b036a0ee4d7f4615
Martin Bentley <martinbentley@btinternet.com>
2018-06-15 09:57:03 +0100

    Added new migration

--- src/ManageCourses.Domain/Migrations/20180615074016_Changed Field Names.Designer.cs
+++ src/ManageCourses.Domain/Migrations/20180615074016_Changed Field Names.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace GovUk.Education.ManageCourses.Domain.Migrations
 {
     [DbContext(typeof(ManageCoursesDbContext))]
-    partial class ManageCoursesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180615074016_Changed Field Names")]
+    partial class ChangedFieldNames
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
