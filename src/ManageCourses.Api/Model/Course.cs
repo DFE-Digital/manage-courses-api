9ba197e1b485da24bdd05c82419d372e7cd33836
Martin Bentley <martinbentley@btinternet.com>
2018-06-22 12:19:21 +0100

    returning new course object+variants

---
+++ src/ManageCourses.Api/Model/Course.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GovUk.Education.ManageCourses.Api.Model
+{
+    public class Course
+    {
+        public string Title { get; set; }
+        public string UcasCode { get; set; }
+        public List<Variant> Variants { get; set; }
+    }
+}
