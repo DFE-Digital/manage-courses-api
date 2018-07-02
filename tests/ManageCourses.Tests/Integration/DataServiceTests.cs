87acbdbab83e3bac7f6ec843a34e5875367a3809
De Fong <de_fong2k@yahoo.com>
2018-07-02 15:04:26 +0100

    Added unit tests

--- tests/ManageCourses.Tests/Integration/DataServiceTests.cs
+++ tests/ManageCourses.Tests/Integration/DataServiceTests.cs
@@ -5,6 +5,9 @@ using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
 using GovUk.Education.ManageCourses.Tests.Integration.DatabaseAccess;
 using GovUk.Education.ManageCourses.Api.Data;
+using GovUk.Education.ManageCourses.Api.Model;
+using GovUk.Education.ManageCourses.Domain.DatabaseAccess;
+using GovUk.Education.ManageCourses.Domain.Models;
 
 namespace GovUk.Education.ManageCourses.Tests.Integration
 {
@@ -15,21 +18,130 @@ namespace GovUk.Education.ManageCourses.Tests.Integration
     public class DataServiceTests : ManageCoursesDbContextIntegrationBase
     {
         public IDataService Subject = null;
+        public IManageCoursesDbContext Context = null;
+
+        private const string TestUserEmail_1 = "email_1@test-manage-courses.gov.uk";
+        private const string TestUserEmail_2 = "email_2@test-manage-courses.gov.uk";
+        private const string TestUserEmail_3 = "email_3@test-manage-courses.gov.uk";
+        
 
         [SetUp]
         public void Setup()
         {
-            Subject = new DataService(this.GetContext());
+            Context = this.GetContext();
+            Subject = new DataService(this.Context);
         }
 
-        //[Test]
-        //public void GetCoursesForUser_isNull()
-        //{
-        //    var email = "email@test.com";
+        [Test]
+        public void ProcessPayload()
+        {
+            var payload = GetPayload();
 
+            Subject.ProcessPayload(payload);
+
+
+            foreach (var item in Context.McUsers)
+            {
+                Assert.IsTrue(payload.Users.Any(
+                    x => x.FirstName == item.FirstName &&
+                    x.LastName == item.LastName &&
+                    x.Email == item.Email));
+
+
+            }
+
+            //GetCoursesForUser_isNull(TestUserEmail_1);
+            //GetCoursesForUser_isNull(TestUserEmail_2);
+            //GetCoursesForUser_isNull(TestUserEmail_3);
+
+        }
+
+        public Payload GetPayload()
+        {
+            var users = new List<McUser>()
+            {
+                new McUser
+                {
+                    FirstName = "FirstName_1",
+                    LastName = "LastName_1",
+                    Email = TestUserEmail_1
+                },
+                new McUser
+                {
+                    FirstName = "FirstName_2",
+                    LastName = "LastName_2",
+                    Email = TestUserEmail_2
+                },
+                new McUser
+                {
+                    FirstName = "FirstName_3",
+                    LastName = "LastName_3",
+                    Email = TestUserEmail_3
+                }
+            };
+            var organisations = new List<McOrganisation> {
+                new McOrganisation {
+                    OrgId = "OrgId_1"
+                },
+                new McOrganisation {
+                    OrgId = "OrgId_2"
+                }
+
+            };
+
+            var institutions = new List<UcasInstitution>
+            {
+                new UcasInstitution {
+                    InstCode = "InstCode_1"
+                },
+                new UcasInstitution {
+                    InstCode = "InstCode_2"
+                }
+            };
+            var mappers = new List<ProviderMapper> {
+                new ProviderMapper () {
+                    OrgId = organisations[1].OrgId,
+                    UcasCode = "UcasCode_1" 
+                }
+            };
+
+            var organisationInstitutions = new List<McOrganisationInstitution>
+            {
+                new McOrganisationInstitution {
+                    InstitutionCode = institutions[1].InstCode,
+                    OrgId = organisations[1].OrgId
+                }
+            };
+            var organisationUsers = new List<McOrganisationUser>
+            {
+                new McOrganisationUser {
+                    Email = TestUserEmail_2,
+                },
+                new McOrganisationUser {
+                    Email = TestUserEmail_3,
+                    OrgId = "OrgId_1"
+                }
+            };
+            var result = new Payload()
+            {
+                Users = users,
+                OrganisationInstitutions = organisationInstitutions,
+                OrganisationUsers = organisationUsers,
+                Organisations = organisations,
+                Institutions = institutions,
+                Mappers = mappers
+            };
+
+            return result;
+        }
+
+        //[TestCase("nothing@nowhere.com")]
+        //public void GetCoursesForUser_isNull(string email)
+        //{         
         //    var result = Subject.GetCoursesForUser(email);
 
-        //    CollectionAssert.(result);
+        //    Assert.IsNull(result.OrganisationId);
+        //    Assert.IsNull(result.OrganisationName);
 
         //}
     }
