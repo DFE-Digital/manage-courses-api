ec99849bcdd85a069b4e84d36188d7c86afd4279
the-daniel-rothig <daniel.rothig@gmail.com>
2018-07-18 14:02:24 +0100

    MSBuild poking

--- travis-build.sh
+++ travis-build.sh
@@ -2,6 +2,7 @@
 set -ev
 dotnet restore ./src/ManageCourses.Api/ManageCourses.Api.csproj
 dotnet restore ./src/ManageCourses.Domain/ManageCourses.Domain.csproj
+dotnet restore ./src/ManageCourses.ApiClient/ManageCourses.ApiClient.csproj
 dotnet restore ./tests/ManageCourses.Tests/ManageCourses.Tests.csproj
 
 dotnet test ./tests/ManageCourses.Tests/ManageCourses.Tests.csproj
