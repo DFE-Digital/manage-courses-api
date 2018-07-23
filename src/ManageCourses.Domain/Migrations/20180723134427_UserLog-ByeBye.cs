a43f37c9e9e32b4a85ff5cff5e752c8fab4c3999
Tim Abell <tim@timwise.co.uk>
2018-07-23 14:45:06 +0100

    Add migration UserLogByeBye

---
+++ src/ManageCourses.Domain/Migrations/20180723134427_UserLog-ByeBye.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace GovUk.Education.ManageCourses.Domain.Migrations
+{
+    public partial class UserLogByeBye : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "user_log");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "user_log",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    first_login_date_utc = table.Column<DateTime>(nullable: false),
+                    last_login_date_utc = table.Column<DateTime>(nullable: false),
+                    sign_in_user_id = table.Column<string>(nullable: true),
+                    user_email = table.Column<string>(nullable: true),
+                    user_id = table.Column<int>(nullable: true),
+                    welcome_email_date_utc = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user_log", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_user_log_mc_user_user_id",
+                        column: x => x.user_id,
+                        principalTable: "mc_user",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_log_user_id",
+                table: "user_log",
+                column: "user_id");
+        }
+    }
+}
