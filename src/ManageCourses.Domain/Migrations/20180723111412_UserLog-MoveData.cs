a1407f486e70195e2a7c54b8872b089d176047fd
Tim Abell <tim@timwise.co.uk>
2018-07-23 14:38:56 +0100

    Up migration sql for UserLog data -> McUser

--- src/ManageCourses.Domain/Migrations/20180723111412_UserLog-MoveData.cs
+++ src/ManageCourses.Domain/Migrations/20180723111412_UserLog-MoveData.cs
@@ -8,7 +8,13 @@ namespace GovUk.Education.ManageCourses.Domain.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.Sql(@"update mc_user u set
+                    first_login_date_utc = ul.first_login_date_utc,
+                    last_login_date_utc = ul.last_login_date_utc,
+                    welcome_email_date_utc = ul.welcome_email_date_utc, 
+                    sign_in_user_id = ul.sign_in_user_id
+                from user_log ul
+                where ul.user_id = u.id or ul.user_email = u.email;");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
