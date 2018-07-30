201d89fec4aa38dd55917ebc0575ee473dea54e4
Tim Abell <tim@timwise.co.uk>
2018-07-30 11:27:09 +0100

    Minimal stub tests for simple happy-path of inviting users
    ---
    Pairing with Martin

---
+++ tests/ManageCourses.Tests/UnitTesting/InviteServiceTests.cs
@@ -0,0 +1,44 @@
+﻿using GovUk.Education.ManageCourses.Api.Services;
+using GovUk.Education.ManageCourses.Api.Services.Email;
+using GovUk.Education.ManageCourses.Domain.DatabaseAccess;
+using Moq;
+using NUnit.Framework;
+
+namespace GovUk.Education.ManageCourses.Tests.UnitTesting
+{
+    [TestFixture]
+    public class InviteServiceTests
+    {
+        private UserService _userService;
+        private IClock _clock;
+        private Mock<IManageCoursesDbContext> _context;
+
+        [SetUp]
+        public void Setup()
+        {
+            _context = new Mock<IManageCoursesDbContext>();
+            _clock = new Mock<IClock>().Object;
+            _userService = new UserService(_context.Object, new Mock<IWelcomeEmailService>().Object, _clock);
+        }
+
+        [Test]
+        public void InviteEmailSent()
+        {
+        }
+
+        [Test]
+        public void InviteDateRecorded()
+        {
+        }
+
+        [Test]
+        public void InviteEmailNotResent()
+        {
+        }
+
+        [Test]
+        public void ThrowsForUnknownMcUser()
+        {
+        }
+    }
+}
