f82ec81230b23482d8f043198b5d2b92f55c0af1
Tim Abell <tim@timwise.co.uk>
2018-07-31 14:49:06 +0100

    Factor out integration test db context building for reuse

--- tests/ManageCourses.Tests/Integration/DatabaseAccess/DbIntegrationTestBase.cs
+++ tests/ManageCourses.Tests/Integration/DatabaseAccess/DbIntegrationTestBase.cs
@@ -1,11 +1,9 @@
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using GovUk.Education.ManageCourses.Api;
 using GovUk.Education.ManageCourses.Domain.DatabaseAccess;
+using GovUk.Education.ManageCourses.Tests.TestUtilities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
-using Microsoft.Extensions.Configuration;
 using NUnit.Framework;
 
 namespace GovUk.Education.ManageCourses.Tests.Integration.DatabaseAccess
@@ -22,16 +20,7 @@ namespace GovUk.Education.ManageCourses.Tests.Integration.DatabaseAccess
 
         protected ManageCoursesDbContext GetContext()
         {
-            var config = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("integration-tests.json")
-                .Build();
-
-            var options = new DbContextOptionsBuilder<ManageCoursesDbContext>()
-                .UseNpgsql(Startup.GetConnectionString(config))
-                .Options;
-
-            return new ManageCoursesDbContext(options);
+            return ContextLoader.GetDbContext(ContextLoader.IntegrationTestJson);
         }
 
         [OneTimeSetUp]
---
+++ tests/ManageCourses.Tests/TestUtilities/ContextLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using GovUk.Education.ManageCourses.Api;
+using GovUk.Education.ManageCourses.Domain.DatabaseAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace GovUk.Education.ManageCourses.Tests.TestUtilities
+{
+    internal class ContextLoader
+    {
+        public const string IntegrationTestJson = "integration-tests.json";
+
+        /// <summary>
+        /// Configures and returns a manage-courses dbContext
+        /// </summary>
+        /// <param name="configFilename">name of json file to use for config data</param>
+        public static ManageCoursesDbContext GetDbContext(string configFilename)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(configFilename)
+                .Build();
+
+            var options = new DbContextOptionsBuilder<ManageCoursesDbContext>()
+                .UseNpgsql(Startup.GetConnectionString(config))
+                .Options;
+
+            return new ManageCoursesDbContext(options);
+        }
+    }
+}
