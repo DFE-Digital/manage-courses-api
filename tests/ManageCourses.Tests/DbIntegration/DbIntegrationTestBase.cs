70ed239b415056ec2efb4966518ab3e0ddc7e7eb
Tim Abell <tim@timwise.co.uk>
2018-08-02 12:06:47 +0100

    Truncate all tables at start of every integration test
    Fixes flapping test that was failing due to data left behind by other tests.

--- tests/ManageCourses.Tests/DbIntegration/DbIntegrationTestBase.cs
+++ tests/ManageCourses.Tests/DbIntegration/DbIntegrationTestBase.cs
@@ -27,6 +27,22 @@ namespace GovUk.Education.ManageCourses.Tests.DbIntegration
         [SetUp]
         public void SetUp()
         {
+            // get a fresh context every time to avoid stale in-memory data contaminating subsequent tests
+            Context = ContextLoader.GetDbContext(Config);
+            // Truncate (delete all data from) all tables, following FK constraints by virtue of CASCADE
+            // https://stackoverflow.com/questions/2829158/truncating-all-tables-in-a-postgres-database/12082038#12082038
+            Context.Database.ExecuteSqlCommandAsync(@"
+                DO
+                $func$
+                BEGIN
+                   EXECUTE
+                   (SELECT 'TRUNCATE TABLE ' || string_agg(oid::regclass::text, ', ') || ' CASCADE'
+                    FROM   pg_class
+                    WHERE  relkind = 'r'  -- only tables
+                    AND    relnamespace = 'public'::regnamespace
+                   );
+                END
+                $func$;").Wait();
         }
     }
 }
