e4c5adeaef8951753cb3292b20aeea4a7c3b0ee5
Tim Abell <tim@timwise.co.uk>
2018-08-03 17:00:32 +0100

    Add controller test Invite_UnknownUser_Returns422
    Fails because Response not mocked yet

--- tests/ManageCourses.Tests/UnitTesting/Controllers/InviteControllerTests.cs
+++ tests/ManageCourses.Tests/UnitTesting/Controllers/InviteControllerTests.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using GovUk.Education.ManageCourses.Api.Controllers;
+using GovUk.Education.ManageCourses.Api.Exceptions;
 using GovUk.Education.ManageCourses.Api.Services;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
@@ -14,6 +15,16 @@ namespace GovUk.Education.ManageCourses.Tests.UnitTesting.Controllers
         public void Invite_Returns200()
         {
             var inviteServiceMock = new Mock<IInviteService>(); // default mock is enough to accecpt call to Invite() without error
+            var inviteController = new InviteController(inviteServiceMock.Object);
+            var result = inviteController.Index("foo@example.org");
+            result.Should().BeOfType<OkResult>();
+        }
+
+        [Test]
+        public void Invite_UnknownUser_Returns422()
+        {
+            var inviteServiceMock = new Mock<IInviteService>();
+            inviteServiceMock.Setup(s => s.Invite(It.IsAny<string>())).Throws<McUserNotFoundException>();
             var inviteController = new InviteController(inviteServiceMock.Object);
             var result = inviteController.Index("foo@example.org");
             result.Should().BeOfType<OkResult>();
