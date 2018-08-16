3812e00446ba1124d53a16694dcaaa44a669caa2
Martin Bentley <martinbentley@btinternet.com>
2018-08-16 12:34:45 +0100

    Added smoke test

--- tests/ManageCourses.Tests/SmokeTests/ApiEndpointTests.cs
+++ tests/ManageCourses.Tests/SmokeTests/ApiEndpointTests.cs
@@ -80,6 +80,23 @@ namespace GovUk.Education.ManageCourses.Tests.SmokeTests
             };
             await apiClient.Enrichment_SaveInstitutionAsync(ucasInstitutionCode, model);
         }
+        [Test]
+        public async Task EnrichmentPublishTest()
+        {
+            SetupSmokeTestData();
+            var apiClient = BuildSigninAwareClient();
+            const string ucasInstitutionCode = "ABC";
+            var model = new UcasInstitutionEnrichmentPostModel();
+            model.EnrichmentModel = new InstitutionEnrichmentModel
+            {
+                TrainWithUs = "wqeqwe",
+                TrainWithDisability = "werwer"
+            };
+            await apiClient.Enrichment_SaveInstitutionAsync(ucasInstitutionCode, model);
+
+            var result = await apiClient.Enrichment_PublishAsync(ucasInstitutionCode);
+            result.Should().NotBeNull();
+        }
 
         [Test]
         public async Task EnrichmentLoadTest()
