628421d3ce9ebadf48912b06d95c8ad5ab984dd8
Tim Abell <tim@timwise.co.uk>
2018-08-17 18:33:31 +0100

    New controller for testing out error conditions

---
+++ src/ManageCourses.Api/Controllers/DevOpsController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using GovUk.Education.ManageCourses.Api.Middleware;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GovUk.Education.ManageCourses.Api.Controllers
+{
+    /// <summary>
+    /// For supportability in operational environment.
+    /// Use the actions in here to verify that various error conditions are correctly logged / alerted etc.
+    /// </summary>
+    [Route("api/[controller]")]
+    [Authorize(AuthenticationSchemes = BearerTokenApiKeyDefaults.AuthenticationScheme)]
+    public class DevOpsController : Controller
+    {
+        /// <summary>
+        /// easily searchable string
+        /// </summary>
+        private const string MagicString = "Zoinks Scooby!";
+
+        private readonly ILogger<DevOpsController> _logger;
+
+        public DevOpsController(ILogger<DevOpsController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Hit this to test exception logging. Always throws.
+        /// </summary>
+        [Route("throw")]
+        public ActionResult Throw()
+        {
+            throw new Exception($"This is a test exception. {MagicString}");
+        }
+
+        /// <summary>
+        /// Spits out log at every level for testing logging configuration.
+        /// </summary>
+        [Route("log")]
+        public ActionResult Log()
+        {
+            _logger.LogCritical($"logtests: Critical. {MagicString}");
+            _logger.LogError($"logtests: Error. {MagicString}");
+            _logger.LogWarning($"logtests: Warning. {MagicString}");
+            _logger.LogInformation($"logtests: Info. {MagicString}");
+            _logger.LogDebug($"logtests: Debug. {MagicString}");
+            _logger.LogTrace($"logtests: Trace. {MagicString}");
+            return Ok();
+        }
+
+        /// <summary>
+        /// Hit this to test logging of server errors.
+        /// No exceptions used, just returns 500 status code.
+        /// </summary>
+        /// <returns></returns>
+        [Route("error")]
+        public ActionResult ServerError()
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+}
