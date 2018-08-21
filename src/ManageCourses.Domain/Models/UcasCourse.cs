3cdaa57cbedc9e1ccc72fc1e56f0d53f73e13d75
Martin Bentley <martinbentley@btinternet.com>
2018-08-21 12:12:11 +0100

    Added fields to Course

--- src/ManageCourses.Domain/Models/UcasCourse.cs
+++ src/ManageCourses.Domain/Models/UcasCourse.cs
@@ -13,8 +13,10 @@ namespace GovUk.Education.ManageCourses.Domain.Models
         public string ProgramType { get; set; }
         public string AccreditingProvider { get; set; }
         public string CrseOpenDate { get; set; }
+        public string Publish { get; set; }
         public string Status { get; set; }
         public string VacStatus { get; set; }
+        public string HasBeenPublished { get; set; }
 
 
         public UcasInstitution UcasInstitution { get; set; }
