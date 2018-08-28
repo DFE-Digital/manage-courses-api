cd1c403a3cdea908fc0a4e5cb994b01ea304da35
the-daniel-rothig <daniel.rothig@gmail.com>
2018-08-28 15:49:27 +0100

    add test

---
+++ tests/ManageCourses.Tests/UnitTesting/Controllers/HomeControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using GovUk.Education.ManageCourses.Api.Controllers;
+using GovUk.Education.ManageCourses.Api.Exceptions;
+using GovUk.Education.ManageCourses.Api.Services.Invites;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+
+namespace GovUk.Education.ManageCourses.Tests.UnitTesting.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        [Test]
+        public void Index()
+        {
+            var controller = new HomeController();
+            var res = controller.Index() as RedirectResult;
+
+            Assert.NotNull(res);
+            Assert.AreEqual("/swagger", res.Url);
+        }
+    }
+}
