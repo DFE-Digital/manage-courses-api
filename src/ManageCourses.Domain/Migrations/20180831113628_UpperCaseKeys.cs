39bd503e37c32bc4046be34b50aa10c95f2e0b9c
Tim Abell <tim@timwise.co.uk>
2018-08-31 12:44:42 +0100

    Migration to normalise case of enrichment database keys

---
+++ src/ManageCourses.Domain/Migrations/20180831113628_UpperCaseKeys.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GovUk.Education.ManageCourses.Domain.Migrations
+{
+    public partial class UpperCaseKeys : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                update course_enrichment
+                set inst_code = upper(inst_code),
+                  ucas_course_code = upper(ucas_course_code);
+            ");
+            migrationBuilder.Sql(@"
+                update institution_enrichment
+                set inst_code = upper(inst_code);
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                update course_enrichment
+                set inst_code = lower(inst_code),
+                  ucas_course_code = lower(ucas_course_code);
+            ");
+            migrationBuilder.Sql(@"
+                update institution_enrichment
+                set inst_code = lower(inst_code);
+            ");
+        }
+    }
+}
