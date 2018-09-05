ae391e312c4d62b85a0c495df7e0b50c46824ef8
Tim Abell <tim@timwise.co.uk>
2018-09-05 15:54:36 +0100

    Scaffolded migration

--- src/ManageCourses.Domain/Migrations/20180905144836_CascadeUserOrg.Designer.cs
+++ src/ManageCourses.Domain/Migrations/20180905144836_CascadeUserOrg.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace GovUk.Education.ManageCourses.Domain.Migrations
 {
     [DbContext(typeof(ManageCoursesDbContext))]
-    partial class ManageCoursesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180905144836_CascadeUserOrg")]
+    partial class CascadeUserOrg
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/ManageCourses.Domain/Migrations/20180905144836_CascadeUserOrg.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace GovUk.Education.ManageCourses.Domain.Migrations
+{
+    public partial class CascadeUserOrg : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_mc_organisation_user_mc_user_email",
+                table: "mc_organisation_user");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_mc_organisation_user_mc_user_email",
+                table: "mc_organisation_user",
+                column: "email",
+                principalTable: "mc_user",
+                principalColumn: "email",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_mc_organisation_user_mc_user_email",
+                table: "mc_organisation_user");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_mc_organisation_user_mc_user_email",
+                table: "mc_organisation_user",
+                column: "email",
+                principalTable: "mc_user",
+                principalColumn: "email",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
