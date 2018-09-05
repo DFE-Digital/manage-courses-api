17a57d693b86633dea7fe76ba123f186a56eca26
Tim Abell <tim@timwise.co.uk>
2018-09-05 15:54:22 +0100

    Cascade delete user -> org-user

--- src/ManageCourses.Domain/DatabaseAccess/ManageCoursesDbContext.cs
+++ src/ManageCourses.Domain/DatabaseAccess/ManageCoursesDbContext.cs
@@ -30,7 +30,8 @@ namespace GovUk.Education.ManageCourses.Domain.DatabaseAccess
                 .HasOne(ou => ou.McUser)
                 .WithMany(u => u.McOrganisationUsers)
                 .HasForeignKey(ou => ou.Email)
-                .HasPrincipalKey(u => u.Email);
+                .HasPrincipalKey(u => u.Email)
+                .OnDelete(DeleteBehavior.Cascade);
             modelBuilder.Entity<McOrganisationUser>()
                 .HasOne(ou => ou.McOrganisation)
                 .WithMany(u => u.McOrganisationUsers)
