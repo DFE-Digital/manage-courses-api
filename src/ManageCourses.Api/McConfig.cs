06d587945e548fc7f00ee48ace272aebd0ae391f
Tim Abell <tim@timwise.co.uk>
2018-09-13 12:12:08 +0100

    Use in props McConfig

--- src/ManageCourses.Api/McConfig.cs
+++ src/ManageCourses.Api/McConfig.cs
@@ -16,18 +16,25 @@ namespace GovUk.Education.ManageCourses.Api
         /// </summary>
         public string BuildConnectionString()
         {
-            var server = _configuration["MANAGE_COURSES_POSTGRESQL_SERVICE_HOST"];
-            var port = _configuration["MANAGE_COURSES_POSTGRESQL_SERVICE_PORT"];
+            var server = PgServer;
+            var port = PgPort;
 
-            var user = _configuration["PG_USERNAME"];
-            var pword = _configuration["PG_PASSWORD"];
-            var dbase = _configuration["PG_DATABASE"];
+            var user = PgUser;
+            var pword = PgPassword;
+            var dbase = PgDatabase;
 
             var sslDefault = "SSL Mode=Prefer;Trust Server Certificate=true";
-            var ssl = _configuration["PG_SSL"] ?? sslDefault;
+            var ssl = PgSsl ?? sslDefault;
 
             var connectionString = $"Server={server};Port={port};Database={dbase};User Id={user};Password={pword};{ssl}";
             return connectionString;
         }
+
+        private string PgServer => _configuration["MANAGE_COURSES_POSTGRESQL_SERVICE_HOST"];
+        private string PgPort => _configuration["MANAGE_COURSES_POSTGRESQL_SERVICE_PORT"];
+        private string PgDatabase => _configuration["PG_DATABASE"];
+        private string PgUser => _configuration["PG_USERNAME"];
+        private string PgPassword => _configuration["PG_PASSWORD"];
+        private string PgSsl => _configuration["PG_SSL"];
     }
 }
