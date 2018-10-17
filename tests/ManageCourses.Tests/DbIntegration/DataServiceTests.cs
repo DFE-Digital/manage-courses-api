cf5b44ea94fd4f22256dcf17002d0e7ee8c28ac2
De Fong <de_fong2k@yahoo.com>
2018-10-17 10:53:12 +0100

    Removed unneeded comments

--- tests/ManageCourses.Tests/DbIntegration/DataServiceTests.cs
+++ tests/ManageCourses.Tests/DbIntegration/DataServiceTests.cs
@@ -238,7 +238,7 @@ namespace GovUk.Education.ManageCourses.Tests.DbIntegration
             Assert.IsTrue(orgList.Count == numOrgs);
             Assert.IsTrue(orgList.All(c => c.TotalCourses == numCourses));
 
-            foreach (var org in orgList)//we have a valist list of data
+            foreach (var org in orgList)
             {
                 var result = DataService.GetOrganisationForUser("anyone@testing.com", org.UcasCode);//try to get the organisation using an invalid email
                 Assert.IsNull(result);
@@ -255,7 +255,7 @@ namespace GovUk.Education.ManageCourses.Tests.DbIntegration
             Assert.IsTrue(orgList.Count == numOrgs);
             Assert.IsTrue(orgList.All(c => c.TotalCourses == numCourses));
 
-            foreach (var org in orgList)//we have a valist list of data
+            foreach (var org in orgList)
             {
                 var result = DataService.GetCourses(TestUserEmail1, org.UcasCode);//get the course for each org
                 Assert.AreEqual(numCourses, result.Courses.Count);
@@ -270,7 +270,7 @@ namespace GovUk.Education.ManageCourses.Tests.DbIntegration
             var orgList = DataService.GetOrganisationsForUser(TestUserEmail1).ToList();
             orgList.Count.Should().Be(numOrgs);
 
-            foreach (var org in orgList)//we have a valist list of data
+            foreach (var org in orgList)
             {
                 var result = DataService.GetCourses(TestUserEmail1, org.UcasCode);//get the course for each org
 
@@ -290,7 +290,7 @@ namespace GovUk.Education.ManageCourses.Tests.DbIntegration
             var orgList = DataService.GetOrganisationsForUser(TestUserEmail1).ToList();
             orgList.Count.Should().Be(numOrgs);
 
-            foreach (var org in orgList)//we have a valist list of data
+            foreach (var org in orgList)
             {
                 var result = DataService.GetCourses(TestUserEmail1, org.UcasCode);//get the course for each org
 
@@ -330,7 +330,7 @@ namespace GovUk.Education.ManageCourses.Tests.DbIntegration
             Assert.IsTrue(orgList.Count == numOrgs);
             Assert.IsTrue(orgList.All(c => c.TotalCourses == numCourses));
 
-            foreach (var org in orgList)//we have a valist list of data
+            foreach (var org in orgList)
             {
                 var result = DataService.GetCourses("anyone@anywhere.com", org.UcasCode);//get the course for each org
                 Assert.True(result.Courses.Count == 0);
@@ -362,7 +362,7 @@ namespace GovUk.Education.ManageCourses.Tests.DbIntegration
             Assert.IsTrue(orgList.Count == numOrgs);
             Assert.IsTrue(orgList.All(c => c.TotalCourses == numCourses));
 
-            foreach (var org in orgList)//we have a valist list of data
+            foreach (var org in orgList)
             {
                 var coursesList = DataService.GetCourses(TestUserEmail1, org.UcasCode);//get the courses for each org
                 foreach (var course in coursesList.Courses)
@@ -383,7 +383,7 @@ namespace GovUk.Education.ManageCourses.Tests.DbIntegration
             Assert.IsTrue(orgList.Count == numOrgs);
             Assert.IsTrue(orgList.All(c => c.TotalCourses == numCourses));
 
-            foreach (var org in orgList)//we have a valist list of data
+            foreach (var org in orgList)
             {
                 var coursesList = DataService.GetCourses(TestUserEmail1, org.UcasCode);//get the courses for each org
                 foreach (var course in coursesList.Courses)
