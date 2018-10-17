80c0b0a56da5d59fd363e840b58a233b92d15c2f
Tim Abell <tim@timwise.co.uk>
2018-10-17 16:16:21 +0100

    Up timeout to 2 hours
    Don't think 15mins will be enough on prod

--- src/ManageCourses.CourseExporterUtil/Publisher.cs
+++ src/ManageCourses.CourseExporterUtil/Publisher.cs
@@ -42,7 +42,7 @@ namespace GovUk.Education.ManageCourses.CourseExporterUtil
             using (var httpClient = new HttpClient())
             {
                 httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _mcConfig.SearchAndCompareApiKey);
-                httpClient.Timeout = TimeSpan.FromMinutes(15);
+                httpClient.Timeout = TimeSpan.FromMinutes(120);
                 var client = new SearchAndCompareApi(httpClient, _mcConfig.SearchAndCompareApiUrl);
                 Console.WriteLine($"Sending {courses.Count()} courses to Search API...");
                 var success = await client.SaveCoursesAsync(courses);
