cdef25464c4f1f0c629c49e66313e46eb249b5b9
the-daniel-rothig <daniel.rothig@gmail.com>
2018-10-23 17:52:00 +0100

    Check out how much dead code I found

--- src/ManageCourses.Api/Services/Data/IDataService.cs
+++ src/ManageCourses.Api/Services/Data/IDataService.cs
@@ -7,10 +7,8 @@ namespace GovUk.Education.ManageCourses.Api.Data
     public interface IDataService
     {
         void ProcessUcasPayload(UcasPayload payload);
-        OrganisationCourses GetCoursesForUserOrganisation(string email, string ucasCode); 
         IEnumerable<UserOrganisation> GetOrganisationsForUser(string email);
         UserOrganisation GetOrganisationForUser(string email, string instCode);
-        void ProcessReferencePayload(ReferenceDataPayload payload);
         Course GetCourse(string email, string instCode, string ucasCode);
         InstitutionCourses GetCourses(string email, string instCode);
         UcasInstitution GetUcasInstitutionForUser(string name, string instCode);
---
+++ tests/ManageCourses.Tests/SmokeTests/ReferenceDataBuilder.cs
@@ -0,0 +1,41 @@
+
+using System;
+using GovUk.Education.ManageCourses.Domain.DatabaseAccess;
+using GovUk.Education.ManageCourses.Domain.Models;
+
+namespace GovUk.Education.ManageCourses.Tests.SmokeTests
+{
+    public static class ReferenceDataExtensions
+    {
+        public static void AddTestReferenceData(this ManageCoursesDbContext context, string username)
+        {
+            context.McUsers.Add(new McUser{
+                    FirstName = "Joe",
+                    LastName = "Bloggs",
+                    Email = username,
+                    AcceptTermsDateUtc = DateTime.UtcNow
+                });
+
+            context.McOrganisations.Add(new McOrganisation {
+                    Name = "Joe's school",
+                    OrgId = "123"
+                });
+                
+            context.UcasInstitutions.Add(new UcasInstitution { 
+                    InstFull = "Joe's school @ UCAS",
+                    InstCode = "ABC"
+                });
+            
+            context.McOrganisationUsers.Add(new McOrganisationUser {
+                    Email = username,
+                    OrgId = "123"
+                });
+            
+            context.McOrganisationIntitutions.Add(new McOrganisationInstitution {
+                    InstitutionCode = "ABC",
+                    OrgId = "123"
+                });
+            
+        }
+    }
+}
