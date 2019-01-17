5921cef32430f609cc33b35d45f29d0b65932190
Tim Abell <tim@timwise.co.uk>
2019-01-17 12:32:48 +0000

    Remove duplicate using

--- src/ManageCourses.Api/Startup.cs
+++ src/ManageCourses.Api/Startup.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System;
 using System.Reflection;
 using GovUk.Education.ManageCourses.Api.ActionFilters;
 using GovUk.Education.ManageCourses.Api.Data;
