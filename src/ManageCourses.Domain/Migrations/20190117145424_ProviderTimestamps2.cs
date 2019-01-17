ed975afdec02bf7f9ef5ff47a191099d4b9ae695
Tim Abell <tim@timwise.co.uk>
2019-01-17 14:57:48 +0000

    Migration for new timestamp fields
    Set default value to avoid failure creating non-nullable column.

---
+++ src/ManageCourses.Domain/Migrations/20190117145424_ProviderTimestamps2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GovUk.Education.ManageCourses.Domain.Migrations
+{
+    public partial class ProviderTimestamps2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "created_at",
+                table: "site",
+                nullable: false,
+                defaultValueSql: "CURRENT_DATE");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "updated_at",
+                table: "site",
+                nullable: false,
+                defaultValueSql: "CURRENT_DATE");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "created_at",
+                table: "provider",
+                nullable: false,
+                defaultValueSql: "CURRENT_DATE");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "updated_at",
+                table: "provider",
+                nullable: false,
+                defaultValueSql: "CURRENT_DATE");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "created_at",
+                table: "site");
+
+            migrationBuilder.DropColumn(
+                name: "updated_at",
+                table: "site");
+
+            migrationBuilder.DropColumn(
+                name: "created_at",
+                table: "provider");
+
+            migrationBuilder.DropColumn(
+                name: "updated_at",
+                table: "provider");
+        }
+    }
+}
