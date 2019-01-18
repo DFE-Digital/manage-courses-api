cf9e4ca69c886a79afd4498bdce78498329dbaea
Tim Abell <tim@timwise.co.uk>
2019-01-18 14:40:58 +0000

    Make webjob zip quieter

--- travis-deploy-sc-exporter.sh
+++ travis-deploy-sc-exporter.sh
@@ -10,7 +10,7 @@ echo "running dotnet publish..."
 dotnet publish src/ManageCourses.CourseExporterUtil --configuration Release
 
 echo "creating zip..."
-zip $deployZip -r src/ManageCourses.CourseExporterUtil/bin/Release/netcoreapp2.1/publish
+zip $deployZip -qr src/ManageCourses.CourseExporterUtil/bin/Release/netcoreapp2.1/publish
 
 echo "uploading to azure WebJob..."
 curl -X PUT -u "$1" --data-binary @$deployZip --header "Content-Type: application/zip" --header "Content-Disposition: attachment; filename=$deployZip" https://$2.scm.azurewebsites.net/api/triggeredwebjobs/sc-exporter/
