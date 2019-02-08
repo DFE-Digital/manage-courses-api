adf97c6a2a59b6a81f3933e33544862f36a1f90f
Tim Abell <tim@timwise.co.uk>
2019-02-08 12:27:15 +0000

    Add LastPublishedAt to Provider
    This is a more normalized location than on the enrichment records.
    This will make creating the new api that needs to queries this for
    recently modified (and published) changes simpler.
    This won't reflect changes from the importer as that's going away.

---
+++ src/ManageCourses.Domain/Migrations/20190208122643_AddProviderLastPublished.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GovUk.Education.ManageCourses.Domain.Migrations
+{
+    public partial class AddProviderLastPublished : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "last_published_at",
+                table: "provider",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "last_published_at",
+                table: "provider");
+        }
+    }
+}
