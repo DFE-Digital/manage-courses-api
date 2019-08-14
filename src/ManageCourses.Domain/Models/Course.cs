0a38852b7af1e0d4ea296d283be15b027eda6255
Tim Abell <tim@timwise.co.uk>
2019-08-14 15:31:32 +0100

    [1953] Switch to Course.IsSend attribute
    To match database change made in rails, so that publish to find
    continues to use accurate data.
    
    https://github.com/DFE-Digital/manage-courses-backend/pull/704/files

--- src/ManageCourses.Domain/Models/Course.cs
+++ src/ManageCourses.Domain/Models/Course.cs
@@ -34,7 +34,7 @@ namespace GovUk.Education.ManageCourses.Domain.Models
         public int? Science { get; set; }
 
         public string Subjects => CourseSubjects != null && CourseSubjects.Any() ? string.Join(", ", CourseSubjects.Select(x => x.Subject.SubjectName)) : string.Empty;
-        public bool IsSen => CourseSubjects != null && CourseSubjects.Any(x => "U3".Equals(x.Subject.SubjectCode, StringComparison.InvariantCultureIgnoreCase));
+        public bool IsSend { get; set; }
         public string StudyMode { get; set; }
         public string AgeRange { get; set; }
         public CourseQualification Qualification { get; set; }
